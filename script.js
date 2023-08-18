d739280f3199e17b7a1fde18face9cc0ffab8c6a
Edonak <edouardnakasila69@gmail.com>
2023-08-17 22:21:35 -0700

    updated eval calcul

--- script.js
+++ script.js
@@ -1,187 +1,164 @@
 //import { calculate } from './calculator.js';
+
 // TODO: Faire la manipulation du DOM dans ce fichier
-// Initialisation des éléments du DOM
-const inputElement = document.getElementById("input");
-const calculationElement = document.getElementById("calcul");
-const digitButtons = document.querySelectorAll(".digit");
-const operatorButtons = document.querySelectorAll("#plus, #minus, #times, #divideby");
-const clearButton = document.getElementById("clear");
-const resetButton = document.getElementById("reset");
-const plusMinusButton = document.getElementById("plusoumoins");
-const percentageButton = document.getElementById("percentage");
-const dotButton = document.querySelector(".dot");
-const equalEl = document.querySelector("#equals");
 
 // Déclaration des variables globales
-let input = "";
-let calculation = "";
-let result = null;
-let operator = "";
-let hasDecimal = false;
-let maxLength = 10;
-// Initialisation calculatrice et écouteurs d'événements aux boutons
-function init() {
-  digitButtons.forEach((button) => {
-    button.addEventListener("click", handleDigit);
-  });
-  operatorButtons.forEach((button) => {
-    button.addEventListener("click", handleOperator);
-  });
-  clearButton.addEventListener("click", handleClear);
-  resetButton.addEventListener("click", handleReset);
-  plusMinusButton.addEventListener("click", handlePlusMinus);
-  percentageButton.addEventListener("click", handlePercentage);
-  dotButton.addEventListener("click", handleDigit);
-  equalEl.addEventListener("click", handleEqual);
+let number = ""; 
+let operator = ""; 
+let calculator = ""; 
+let result = 0;
 
-  updateDisplay();
-}
+// Sélectionner les éléments du DOM
+const inputElement = document.getElementById("input"); 
+const calculElement = document.getElementById("calcul"); 
+const reset = document.getElementById("reset"); 
+const clear = document.getElementById("clear"); 
+const plusOuMoins = document.getElementById("plusoumoins"); 
+const percentage = document.getElementById("percentage"); 
+const division = document.getElementById("divideby");
+const multiplicate = document.getElementById("times");
+const soustraction = document.getElementById("minus"); 
+const addition = document.getElementById("plus"); 
+const equalsElement = document.getElementById("equals"); 
+const digits = document.querySelectorAll(".digit"); 
+const dot = document.querySelector(".dot"); 
 
-//conversion des boutons submit
+// Ecouteurs d'événements sur les  boutons
+reset.addEventListener("click", resetAll);
+clear.addEventListener("click", clearInput);
+plusOuMoins.addEventListener("click", changeSign);
+percentage.addEventListener("click", getPercentage);
+division.addEventListener("click", () => addOperator("/"));
+multiplicate.addEventListener("click", () => addOperator("*"));
+soustraction.addEventListener("click", () => addOperator("-"));
+addition.addEventListener("click", () => addOperator("+"));
+equalsElement.addEventListener("click", calculate);
+
 const form = document.querySelector('form');
-form.addEventListener('submit', event => {
-  event.preventDefault();
+form.addEventListener('submit',event =>{
+    event.preventDefault();
 });
-
+// LES FUNCTIONS AUXILIAIRES
+// Mis à jour de l'affichage de l'input et du calcul
 function updateDisplay() {
-  inputElement.value = input;
-  calculationElement.textContent = calculation;
+  input.value = number; 
+  calcul.textContent = calculator;
 }
 
-function handleDigit(event) {
-  const digit = event.target.textContent;
-  if (input === "" || input === result.toString()) {
-    input = "";
-    result = 0;
-    hasDecimal = false;
-  }
-
-  // Vérification si l'entrée est inférieure à la longueur maximale
-  if (input.length < maxLength) {
-    if (digit === ".") {
-      if (!hasDecimal) {
-        input += digit;
-        hasDecimal = true;
-      }
-    } else {
-      input += digit;
-    }
-    updateDisplay();
-  }
+function resetAll() {
+  number = ""; 
+  operator = ""; 
+  calculator = ""; 
+  result = 0; 
+  updateDisplay(); 
 }
 
-// Gestion de l'entrée des opérateurs par l'utilisateur
-function handleOperator(event) {
-  const newOperator = event.target.textContent;
-
-  if (input !== "") {
-    if (calculation === "") {
-      result = parseFloat(input);
-      calculation = input;
-    } else {
-      if (operator !== "") {
-        result = calculate(result, parseFloat(input), operator);
-        calculation += " " + input;
-      }
-    }
-    // Affectation du nouvel opérateur à l'opérateur et au calcul
-    operator = newOperator;
-    calculation += " " + operator;
-
-    if (operator === "=") {
-      input = result.toString();
-      operator = "";
-      calculation = "";
-    } else {
-      input = "";
-      hasDecimal = false;
-    }
-    updateDisplay();
-  }
+function clearInput() {
+  number = ""; 
+  updateDisplay(); 
 }
 
-// Gestion de l'effacement de l'entrée ou du calcul de l'utilisateur
-function handleClear() {
-  if (input !== "") {
-    input = "";
-    hasDecimal = false;
-  } else {
-    calculation = "";
-    operator = "";
-  }
-  updateDisplay();
-}
-
-// Réinitialisation de la calculatrice à son état initial
-function handleReset() {
-  input = "";
-  calculation = "";
-  result = 0;
-  operator = "";
-  hasDecimal = false;
-  updateDisplay();
-}
-
-// Inversion du signe de l'entrée de l'utilisateur
-function handlePlusMinus() {
-  if (input !== "" && input !== "0") {
-    if (input.startsWith("-")) {
-      input = input.slice(1);
-    } else {
-      input = "-" + input;
-    }
-    updateDisplay();
-  }
-}
-
-// Conversion de l'entrée de l'utilisateur en pourcentage
-function handlePercentage() {
-  if (input !== "") {
-    input = (parseFloat(input) / 100).toString();
-    hasDecimal = input.includes(".");
-    updateDisplay();
-  }
-}
-
-// Une fonction qui effectue le calcul et affiche le résultat
-function handleEqual() {
-  if (inputElement && operatorButtons) {
-    calculation += ` ${operatorButtons} ${inputElement}`; 
-    result = eval(calculation); 
-    inputElement = result.toString(); 
-    calculation += " ="; 
-
+function changeSign() {
+  if (number) {
+    number = (-number).toString(); 
     updateDisplay(); 
   }
 }
 
-// Calcul entre deux nombres et un opérateur donnés
-function calculate(num1, num2, op) {
-  let res;
-  switch (op) {
-    case "+":
-      res = num1 + num2;
-      break;
-    case "-":
-      res = num1 - num2;
-      break;
-    case "×":
-      res = num1 * num2;
-      break;
-    case ":":
-      if (num2 === 0) {
-        alert('Impossible de diviser par zéro');
-        return;
-      }
-      res = num1 / num2;
-      break;
-    default:
-      res = num1;
+function getPercentage() {
+  if (calculator) {
+    result /= 100;
+    number = result.toString(); 
+    calculator += " %"; 
+    updateDisplay(); 
   }
-
-  return res;
 }
 
-document.addEventListener("DOMContentLoaded", init);
+// Ajout d'un opérateur au calcul en cours
+function addOperator(op) {
+  if (number) {
+    if (calculator) {
+      calculator += ` ${operator} ${number}`; 
+      result = eval(calculator); 
+    } else {
+      calculator = number;
+      result = Number(number);
+    }
+    number = ""; 
+    operator = op; 
+    updateDisplay();
+  }
+}
 
+// Calcul et affichage des résultats
+function calculate() {
+  if (number && operator) {
+    calculator += ` ${operator} ${number}`; 
+    result = eval(calculator); 
+    number = result.toString(); 
+    calculator += " ="; 
+    updateDisplay(); 
+  }
+}
 
+// Parcours sur les boutons contenant des chiffres
+for (let digit of digits) {
+   digit.addEventListener("click", () => {
+    if (number.length < 10) {
+      if (number === "0") {
+        number = digit.textContent; 
+      } else {
+        number += digit.textContent; 
+      }
+      updateDisplay(); 
+    }
+}); }
+
+// Ecouteur d’événement pour les decimales
+dot.addEventListener("click", () => { 
+    if (number.length < 10) { 
+    if (!number.includes(".")) { 
+        if (number === "") { 
+          number = "0."; 
+        } else { 
+          number += "."; 
+        } updateDisplay(); 
+    } } });
+ 
+input.addEventListener("keydown", (event) => { 
+
+    if (event.key >= "0" && event.key <= "9") { 
+        if (number.length < 10) { 
+            if (number === "0") { 
+              number = event.key; 
+            } else {  
+              number += event.key; 
+            } updateDisplay(); 
+        } 
+    } 
+    else if (event.key === ".") { 
+            if (number.length < 10) { 
+                
+                if (!number.includes(".")) {  
+                    if (number === "") { 
+                      number = "0."; 
+                    } 
+                    else { 
+                      number += ".";  
+                    } updateDisplay();  
+                } 
+            } 
+    } 
+                
+    else if (event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/") { 
+         addOperator(event.key);  
+    } 
+    else if (event.key === "=" || event.key === "Enter") { 
+        calculate(); 
+    } 
+    else if (event.key === "Backspace") {  
+        clearInput();
+    } 
+    else if (event.key === "Escape") {
+        resetAll(); 
+    } });
